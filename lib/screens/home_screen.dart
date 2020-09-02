128e381231e650e649c90d956cfb6896fa7f6121
Farid Esquivel <esquivelfarid@gmail.com>
2020-09-01 21:53:29 -0500

    Addig step by step controls

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -19,6 +19,11 @@ class _HomeScreenState extends State<HomeScreen> {
   List<String> listResultMultiplications = [];
   TextEditingController _c1 = new TextEditingController();
   TextEditingController _c2 = TextEditingController();
+  bool showStepByStep = false;
+  String explanation = "";
+  String carrier = "";
+  int currentStep = 0;
+  int totalSteps = 10;
 
   @override
   void initState() {
@@ -39,20 +44,128 @@ class _HomeScreenState extends State<HomeScreen> {
 
   Widget _body() {
     return SafeArea(
+      child: Stack(
+        children: <Widget>[
+          Container(
+            width: _size.width,
+            color: Colors.green,
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  SizedBox(height: 20.0),
+                  _topInput(),
+                  _buttonContainer(),
+                  _solution(),
+                  SizedBox(height: 60.0),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            height: _size.height,
+          ),
+          _controlsStepByStep(),
+        ],
+      ),
+    );
+  }
+
+  Widget _controlButton(String text, Function f) {
+    return RaisedButton(
+      onPressed: f,
+      child: Text(
+        text,
+        style: TextStyle(
+          color: Colors.white,
+        ),
+      ),
+      color: Colors.orange,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(18.0),
+        side: BorderSide(
+          color: Colors.orange[700],
+        ),
+      ),
+    );
+  }
+
+  Widget _explanation() {
+    if (explanation.length == 0) return Container();
+    var textStyle = TextStyle(
+      color: Colors.black,
+      fontSize: 17.0,
+    );
+    return Container(
+      margin: EdgeInsets.only(bottom: 10.0),
+      height: 60.0,
+      width: _size.width * 0.8,
+      decoration: BoxDecoration(
+        color: Colors.green[300],
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Text(
+            explanation,
+            style: textStyle,
+          ),
+          Text(
+            carrier,
+            style: textStyle,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _controlLabel() {
+    return Container(
+      child: Text(
+        "$currentStep/$totalSteps",
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+          fontSize: 18.0,
+        ),
+      ),
+    );
+  }
+
+  Widget _controlsStepByStep() {
+    if (!showStepByStep) Container();
+    return Positioned(
       child: Container(
         width: _size.width,
-        color: Colors.green,
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            SizedBox(height: 20.0),
-            _topInput(),
-            _buttonContainer(),
-            _solution(),
-            SizedBox(height: 30.0),
+            _explanation(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                _controlButton("Anterior", () {
+                  if (currentStep > 0) {
+                    currentStep--;
+                    getMultiplications(number1, number2);
+                    solve();
+                    setState(() {});
+                  }
+                }),
+                _controlLabel(),
+                _controlButton("Siguiente", () {
+                  if (currentStep < totalSteps) {
+                    currentStep++;
+                    solve();
+                    setState(() {});
+                  }
+                }),
+              ],
+            )
           ],
         ),
       ),
+      bottom: 10,
     );
   }
 
@@ -61,7 +174,7 @@ class _HomeScreenState extends State<HomeScreen> {
       margin: EdgeInsets.only(right: 20.0, bottom: 10.0, top: 5.0),
       alignment: Alignment.centerRight,
       height: 3.0,
-      width: width * 45.0,
+      width: width * 40.0,
       color: Colors.white,
     );
   }
@@ -79,35 +192,33 @@ class _HomeScreenState extends State<HomeScreen> {
       numbersDisplay.add(_oneLine(number2.length));
     }
     String finalResult = (int.parse(number1) * int.parse(number2)).toString();
-    return Expanded(
-      child: Container(
-        width: _size.width * 0.8,
-        alignment: Alignment.centerRight,
-        margin: EdgeInsets.only(
-          top: 20.0,
+    return Container(
+      width: _size.width * 0.8,
+      alignment: Alignment.centerRight,
+      margin: EdgeInsets.only(
+        top: 20.0,
+      ),
+      padding: EdgeInsets.symmetric(vertical: 10.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        border: Border.all(
+          color: Colors.green[300],
+          width: 1.0,
         ),
-        padding: EdgeInsets.symmetric(vertical: 10.0),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10.0),
-          border: Border.all(
-            color: Colors.green[300],
-            width: 1.0,
-          ),
-          color: Colors.green[600],
-        ),
-        child: SingleChildScrollView(
-          physics: BouncingScrollPhysics(),
-          scrollDirection: Axis.horizontal,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: <Widget>[
-              ...numbersDisplay,
-              ..._multiplicationsSteps(),
-              _oneLine(finalResult.length),
-              _line(finalResult)
-            ],
-          ),
+        color: Colors.green[600],
+      ),
+      child: SingleChildScrollView(
+        physics: BouncingScrollPhysics(),
+        scrollDirection: Axis.horizontal,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            ...numbersDisplay,
+            ..._multiplicationsSteps(),
+            /* _oneLine(finalResult.length),
+              _line(finalResult, isResult: true) */
+          ],
         ),
       ),
     );
@@ -115,7 +226,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
   String addSpaces(String val, int size) {
     for (int i = 0; i < size; i++) {
-      val = val + "0";
+      val = val + " ";
     }
     return val;
   }
@@ -125,7 +236,8 @@ class _HomeScreenState extends State<HomeScreen> {
     for (int i = 0; i < listResultMultiplications.length; i++) {
       String m = listResultMultiplications[i];
 
-      if (i == listResultMultiplications.length - 1) {
+      if (i == listResultMultiplications.length - 1 &&
+          listResultMultiplications.length > 1) {
         sizeOfLastMultiplication = m.length;
         result.add(_line("+" + addSpaces(m, i)));
       } else {
@@ -135,18 +247,28 @@ class _HomeScreenState extends State<HomeScreen> {
     return result;
   }
 
-  Widget _line(String text) {
+  Widget _lineItem(String char, {bool isResult = false}) {
     return Container(
-      padding: EdgeInsets.only(right: 20.0),
-      alignment: Alignment.centerRight,
+      width: 35.0,
       child: Text(
-        text,
+        char,
         style: TextStyle(
           color: Colors.white,
           fontSize: 46.0,
-          letterSpacing: 20,
+          fontWeight: isResult ? FontWeight.bold : FontWeight.normal,
         ),
       ),
+    );
+  }
+
+  Widget _line(String text, {bool isResult = false}) {
+    List<Widget> _items = [];
+    for (int i = 0; i < text.length; i++) {
+      _items.add(_lineItem(text[i], isResult: isResult));
+    }
+
+    return Row(
+      children: <Widget>[..._items],
     );
   }
 
@@ -193,40 +315,63 @@ class _HomeScreenState extends State<HomeScreen> {
       n1 = n2x.split('').reversed.toList();
     }
 
-    List<String> acumulados = [];
     int val1;
     int val2;
     int result;
     int aux = 0;
 
     print("------------------------");
+    int currentStepFunction = 0;
     for (int i = 0; i < n1.length; i++) {
       String x1 = n1[i];
       String resultLine = "";
       for (int j = 0; j < n2.length; j++) {
         String x2 = n2[j];
-        print("Multiplicar $x1 x $x2");
         val1 = int.parse(x1);
         val2 = int.parse(x2);
         result = val1 * val2;
+        explanation = "Multiplicar $x1 x $x2 = $result";
         if (aux != 0) {
           result = result + aux;
-          print("Llevabas $aux, result: $result");
+          //print("Llevabas $aux, result: $result");
         }
         if (result > 10 && j != n2.length - 1) {
           aux = (result / 10).floor();
-          print("se lleva $aux");
+          //print("se lleva $aux");
           result = result % 10;
         } else {
           aux = 0;
         }
+
         resultLine = "$result" + resultLine;
+
+        print("C $currentStep == $currentStepFunction");
+        if (currentStep == currentStepFunction) {
+          break;
+        } else {
+          currentStepFunction++;
+        }
       }
 
       steps.add(resultLine);
+      if (currentStep == currentStepFunction) {
+        break;
+      }
     }
 
     return steps;
+  }
+
+  void solve() {
+    isSolve = true;
+    FocusScopeNode currentFocus = FocusScope.of(context);
+    if (!currentFocus.hasPrimaryFocus) {
+      currentFocus.unfocus();
+    }
+    if (number1.length > 0 && number2.length > 0) {
+      listResultMultiplications = getMultiplications(number1, number2);
+    }
+    setState(() {});
   }
 
   Widget _buttonContainer() {
@@ -238,18 +383,7 @@ class _HomeScreenState extends State<HomeScreen> {
           FlatButton(
             color: Colors.orange[600],
             onPressed: () {
-              isSolve = true;
-              FocusScopeNode currentFocus = FocusScope.of(context);
-              if (!currentFocus.hasPrimaryFocus) {
-                currentFocus.unfocus();
-              }
-              if (number1.length > 0 && number2.length > 0) {
-                number1 = "123";
-                number2 = "245";
-                listResultMultiplications =
-                    getMultiplications(number1, number2);
-              }
-              setState(() {});
+              solve();
             },
             child: Text(
               "Multiplicar",
@@ -296,49 +430,6 @@ class _HomeScreenState extends State<HomeScreen> {
             }, _c2),
           )
         ],
-      ),
-    );
-  }
-
-  Widget _table() {
-    return SingleChildScrollView(
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Flexible(
-            child: SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: _buildRows(20),
-              ),
-            ),
-          )
-        ],
-      ),
-    );
-  }
-
-  List<Widget> _buildCells(int count) {
-    return List.generate(
-      count,
-      (index) => Container(
-        alignment: Alignment.center,
-        width: 120.0,
-        height: 60.0,
-        color: Colors.white,
-        margin: EdgeInsets.all(4.0),
-        child:
-            Text("${index + 1}", style: Theme.of(context).textTheme.headline1),
-      ),
-    );
-  }
-
-  List<Widget> _buildRows(int count) {
-    return List.generate(
-      count,
-      (index) => Row(
-        children: _buildCells(10),
       ),
     );
   }
